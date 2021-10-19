c5b0ed5df80571fab760474e7c59ef4417810e63
Vanessa <nessa1vane@icloud.com>
2021-10-18 22:12:54 -0300

    verificação de login

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,18 +1,30 @@
 const { application } = require("express");
-const usuario = [{ login: "MeuLogin", senha: "MinhaSenha"}];
+const usuarios = [{ login: "MeuLogin", senha: "MinhaSenha" }];
+let verlogin, versenha;
 
 module.exports = {
 
     index(req, res) {
         res.send("Tudo certo com o servidor");
     },
-    
+
     login(req, res) {
         res.sendFile(__dirname + '/view' + '/login.html');
     },
 
     confirma(req, res) {
-        
+        let { login, senha } = req.body;
+
+        var verificacao = false;
+        for (let i = 0; i < usuarios.length; i++) {
+            if (usuarios[i].login == login && usuarios[i].senha == senha) {
+                verificacao = true;
+                res.json({ Response: "Você foi logado com sucesso!! Bem vindo: ",login});
+                break;
+            } else {
+                res.json({Response: "Login não efetuado, tente novamente"});
+            }
+        }
     }
 };
 
